ceeb0a32dee08129c483046167e784f69f7fae95
Lalit <lalit.garghate@gmail.com>
2021-05-24 17:57:11 +0530

    enzyme's shallow rendering

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,10 @@
-import Enzyme from "enzyme";
+import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test("renders learn react link", () => {});
+test("renders learn react link", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy();
+});
